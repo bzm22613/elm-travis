bb8291668ce9f6b890284750b0dd6e1326e1fb8c
Masato Tokuda <bzm22613@gmail.com>
2019-04-14 19:15:04 +0900

    github viewer v1

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,36 +1,29 @@
 module Example exposing (suite)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Route exposing (..)
 import Test exposing (..)
+import Url exposing (..)
+
+
+testParse : String -> String -> Maybe Route -> Test
+testParse name path expectedRoute =
+    test name <|
+        \_ ->
+            Url.fromString ("http://example.com" ++ path)
+                |> Maybe.andThen Route.parse
+                |> Expect.equal expectedRoute
 
 
 suite : Test
 suite =
-    describe "The String module"
-        [ describe "String.reverse"
-            -- Nest as many descriptions as you like.
-            [ test "has no effect on a palindrome" <|
-                \_ ->
-                    let
-                        palindrome =
-                            "hannah"
-                    in
-                    Expect.equal palindrome (String.reverse palindrome)
-
-            -- Expect.equal is designed to be used in pipeline style, like this.
-            , test "reverses a known string" <|
-                \_ ->
-                    "ABCDEFG"
-                        |> String.reverse
-                        |> Expect.equal "GFEDCBA"
-
-            -- fuzz runs the test 100 times with randomly-generated inputs!
-            , fuzz string "restores the original string if you run it again" <|
-                \randomlyGeneratedString ->
-                    randomlyGeneratedString
-                        |> String.reverse
-                        |> String.reverse
-                        |> Expect.equal randomlyGeneratedString
-            ]
+    describe "Route"
+        [ testParse "should parse Top" "/" (Just Route.Top)
+        , testParse "should parse Top with queries" "/?dummy=value" (Just Route.Top)
+        , testParse "should parse Top with hash" "/#dummy" (Just Route.Top)
+        , testParse "should parse User" "/foo" (Just (Route.User "foo"))
+        , testParse "should parse Repo"
+            "/foo/bar"
+            (Just (Route.Repo "foo" "bar"))
+        , testParse "should parse invalid path" "/foo/bar/baz" Nothing
         ]
